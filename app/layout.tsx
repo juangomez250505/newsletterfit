335e6c3d3d306e6639d6372be08d76b39a0f1402
juangomez250505 <juangomez250505@gmail.com>
2026-09-04 17:21:11 +0000

    Add homepage canonical

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://getnewsletterfit.com"),
+  alternates: {
+  canonical: "/",
+},
 
   title: "Newsletter Platform Cost Calculator | NewsletterFit",
 
